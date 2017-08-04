637d57f8158c9de91bdc055507b37729e52d94dc
Jay Harel <jharel@kollective.com>
2017-08-03 19:29:58 -0700

    basic scaffolding

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
-console.log("Hello World")
+const express = require("express")
+const app = express()
+
+app.get("/", function(req, res) {
+	res.send("Hello world, it's cold in here")
+})
+
+app.listen(8000,function(){
+	console.log("Running app on port 8000")
+})
